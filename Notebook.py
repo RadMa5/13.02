87befea81dca17670ae3eeeaaf90882615954719
RadMa <13coolguy37@gmail.com>
2024-02-19 11:27:23 +0500

    1/3 cleaned

--- Notebook.py
+++ Notebook.py
@@ -2,44 +2,44 @@ import time
 import csv
 import os
 
-class Notes:
-    id = 1
-    fields = ["id", "head", "body", "date"]
 
-    def write(head: str, body: str):
-        if os.stat("data.csv").st_size != 0: #sets the id counter
-            with open("data.csv", "r", newline = "") as data:
-                fields = ["id", "head", "body", "date"]
-                reader = csv.DictReader(data, fieldnames= fields)
-                for row in reader:
-                    Notes.id += 1
+id = 1
+fields = ["id", "head", "body", "date"]
 
-        with open("data.csv", "a", newline = "") as data:
-            writer = csv.DictWriter(data, fieldnames = Notes.fields)
-            localtime = time.strftime("%b %d %Y %H:%M", time.localtime(time.time())) 
-
-            writer.writerow({"id": Notes.id,"head": head, "body": body, "date": localtime})
-        return
-        
-    def printHeaders():
+def write(head: str, body: str):
+    if os.stat("data.csv").st_size != 0: #sets the id counter
         with open("data.csv", "r", newline = "") as data:
-            reader = csv.DictReader(data, fieldnames = Notes.fields)
+            fields = ["id", "head", "body", "date"]
+            reader = csv.DictReader(data, fieldnames= fields)
             for row in reader:
-                print(row["id"], ". ", row["head"], ". Last changed at ", row["date"])
-        return
+                id += 1
+
+    with open("data.csv", "a", newline = "") as data:
+        writer = csv.DictWriter(data, fieldnames = fields)
+        localtime = time.strftime("%b %d %Y %H:%M", time.localtime(time.time())) 
+
+        writer.writerow({"id": id,"head": head, "body": body, "date": localtime})
+    return
     
-    def printNote(num: int):
-        with open("data.csv", "r", newline = "") as data:
-            reader = csv.DictReader(data, fieldnames = Notes.fields)
-            foundNote = False
-            for row in reader:
-                if row["id"] == num:
-                    print("\n", row["id"], ". ", row["head"], "\n", row["body"], "\nLast changed at ", row["date"])
-                    foundNote = True
-                    break
-            
-            if not foundNote:
-                print("The note wasn't found")
-        return
+def printHeaders():
+    with open("data.csv", "r", newline = "") as data:
+        reader = csv.DictReader(data, fieldnames = fields)
+        for row in reader:
+            print(row["id"], ". ", row["head"], ". Last changed at ", row["date"])
+    return
+
+def printNote(num: int):
+    with open("data.csv", "r", newline = "") as data:
+        reader = csv.DictReader(data, fieldnames = fields)
+        foundNote = False
+        for row in reader:
+            if row["id"] == num:
+                print("\n", row["id"], ". ", row["head"], "\n", row["body"], "\nLast changed at ", row["date"])
+                foundNote = True
+                break
+        
+        if not foundNote:
+            print("The note wasn't found")
+    return
 
 
--- NotebookUI.py
+++ NotebookUI.py
@@ -3,23 +3,26 @@ import Notebook
 fc = open("data.csv", "a", newline="")
 fc.close()
 while True:
-    test1 = True
-    test2 = True
+    incorrectInput = True
 
     print("\nWelcome to the Notebook\n\nList of options:\n1. Show the list of all notes\n2. Open a specific note\n3. Create a new note\n4. Redact an existing note\n5. Delete an existing note\n6. Exit")
+    ch = 0
+    while incorrectInput:
+        try:
+            ch = int(input("Input your choice: "))
+            if ch <= 0 or ch >= 7:
+                print("Incorrect input")
+            else:
+                incorrectInput = False
+        except ValueError:
+            print("Incorrect input")
 
-    ch = int(input("Input your choice: "))
-    while True:
-        if ch <= 0 or ch >= 7:
-            ch = int(input("Input is incorrect, please, try again: "))
-        else:
-            break
 
-    if ch == 1:
-        Notebook.Notes.printHeaders()
-    elif ch == 2:
-        chId = input("Input the number of the note: ")
-        Notebook.Notes.printNote(chId)
+        if ch == 1:
+            Notebook.printHeaders()
+        elif ch == 2:
+            chId = input("Input the number of the note: ")
+            Notebook.printNote(chId)
 
 
 
